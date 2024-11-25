822e94c504ea49b68f0472fef60f366867f9f846
fallingflow <y30660912@gmail.com>
2024-11-25 10:57:08 +0900

    Input Sample Data

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 address = "https://open.api.nexon.com/mabinogi/v1/auction/list"
 API_KEY = "test_3aea5b595556584ab54c0245a7e2a9eabac8e93dac17f6ee655c7eee8787f8cdefe8d04e6d233bd35cf2fabdeb93fb0d"
 
-function getDataByCategory(category, cursor = null) {
+items = []
+
+function getDataByCategory(category, cursor = null, paging = 0) {
     url = address + "?auction_item_category=" + category
+
     if (cursor != null){
-        url += "&cursor" + cursor
+        url += "&cursor=" + cursor
     }
+    console.log(url)
 
     $.ajax({
         method: "GET",
@@ -14,17 +18,28 @@ function getDataByCategory(category, cursor = null) {
             xhr.setRequestHeader("x-nxopen-api-key", API_KEY)
         },
         success: function (res) {
-                for (let i = 0; i < res['auction_item'].length; i++){
-                    console.log(res['auction_item'][i]['date_auction_expire'])
-                }
+            items.push(res['auction_item'])
+            if (paging < 2){
+                paging++
+                getDataByCategory(category, res['next_cursor'], paging)
+            }
         }
     })
+
+    // const blob = new Blob([JSON.stringify(items)], {type: 'application/json'});
+    // const url = URL.createObjectURL(blob);
+    // const a = document.createElement
+    // a.href = url
+    // a.download = 'items.json'
+    // a.click()
+    //
+    // URL.revokeObjectURL(url)
 }
 
 function getDataByName(name, cursor = null){
     url = address + "?item_name=" + name
     if (cursor != null){
-        url += "&cursor" + cursor
+        url += "&cursor=" + cursor
     }
     $.ajax({
         method: "GET",
@@ -38,4 +53,118 @@ function getDataByName(name, cursor = null){
     })
 }
 
-getDataByName('동물 캐릭터 분양 메달')
+function changeTheme(){
+    let theme = document.getElementById('navigator-theme-text')
+    let themeText = theme.innerText
+
+    if (themeText == '밝은 모드로'){
+        theme.innerText = '다크 모드로'
+    }
+    else{
+        theme.innerText = '밝은 모드로'
+    }
+}
+
+function getSampleItems(){
+    fetch('data/items.json')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            drawTable(data)
+        })
+    return []
+}
+
+function parsePrice(price){
+    let priceStr = price.toString()
+    let priceLen = priceStr.length
+    let parsedPrice = ""
+    let cnt = 0
+    for (let i=priceLen-1; i>=0; i--){
+        parsedPrice = priceStr[i] + parsedPrice
+        cnt++
+        if (cnt % 3 == 0 && i != 0){
+            parsedPrice = "," + parsedPrice
+        }
+    }
+    return parsedPrice
+}
+function getItemSimpleInfo(items){
+    let itemNames = []
+    items.forEach(i => {
+        i.forEach(item => {
+            itemNames.push([item['item_name'], item['date_auction_expire'], parsePrice(item['auction_price_per_unit'])])
+        })
+    })
+    console.log(itemNames)
+    return itemNames
+}
+function getItemDetailInfo(items){
+    let itemNames = []
+    items.forEach(i => {
+        i.forEach(item => {
+            itemNames.push([item['item_name'], item['date_auction_expire'], parsePrice(item['auction_price_per_unit'])])
+        })
+    })
+    console.log(itemNames)
+    return itemNames
+}
+function drawTable(items){
+    let itemNames = getItemSimpleInfo(items)
+    let table = document.getElementById('item-list')
+    let tbody = document.createElement('tbody')
+    table.appendChild(tbody)
+
+    for (let i=0; i<50; i++){
+        let tr = document.createElement('tr')
+        tbody.appendChild(tr)
+
+        let td = document.createElement('td')
+        td.innerText = i+1
+        tr.appendChild(td)
+        td = document.createElement('td')
+        td.innerText = itemNames[i][0]
+        tr.appendChild(td)
+        td = document.createElement('td')
+        td.innerText = itemNames[i][2]
+        tr.appendChild(td)
+    }
+    // itemNames.forEach(item => {
+    //
+    //     // item.forEach(i => {
+    //     //     let td = document.createElement('td')
+    //     //     td.innerText = i
+    //     //     tr.appendChild(td)
+    //     //     td = document.createElement('td')
+    //     //     td.innerText = i[0]
+    //     //     tr.appendChild(td)
+    //     //     td = document.createElement('td')
+    //     //     td.innerText = i[2]
+    //     //     tr.appendChild(td)
+    //     // })
+    //
+    //     for (let i=0; i<50; i++){
+    //         let tr = document.createElement('tr')
+    //         tbody.appendChild(tr)
+    //
+    //         let td = document.createElement('td')
+    //         td.innerText = i
+    //         tr.appendChild(td)
+    //         td = document.createElement('td')
+    //         td.innerText = item[i][0]
+    //         tr.appendChild(td)
+    //         td = document.createElement('td')
+    //         td.innerText = item[i][2]
+    //         tr.appendChild(td)
+    //     }
+    // })
+}
+
+$(document).ready(function () {
+    $('#navigator-theme').click(function(){
+        changeTheme()
+    })
+
+    getSampleItems()
+})
